e9dd28dedeba432cac965f82ca4eeec96f0ad6f4
haohao <z1446722092@gmail.com>
2022-04-22 23:48:56 +0800

    add 396. 旋转函数

---
+++ 396.旋转函数.java
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=396 lang=java
+ *
+ * [396] 旋转函数
+ */
+
+// @lc code=start
+/**
+ * 找规律可以得到递推公式
+ * F(k) = F(k-1) + numSum - n * nums[n-k]
+ * numSum 为数组元素之和
+ * n 为数组的长度
+ */
+class Solution {
+    public int maxRotateFunction(int[] nums) {
+        int f = 0;
+        int numsLen = nums.length;
+        int numSum = 0;
+
+        for (int i = 0; i < numsLen; i++) {
+            numSum += nums[i]; // 计算数组所有元素之和
+        }
+
+        for (int i = 0; i < numsLen; i++) {
+            f += i * nums[i]; // 计算 F(0)
+        }
+
+        int res = f;
+        for (int j = numsLen - 1; j > 0; j--) {
+            f += numSum - numsLen * nums[j];
+            res = Math.max(res, f);
+
+        }
+        return res;
+
+    }
+}
+// @lc code=end
+
