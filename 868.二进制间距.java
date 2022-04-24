b4cb1419ed40743fbd1259a175cd5c0690e68656
haohao <z1446722092@gmail.com>
2022-04-24 22:11:28 +0800

    add some comments

--- 868.二进制间距.java
+++ 868.二进制间距.java
@@ -13,13 +13,13 @@ class Solution {
     public int binaryGap(int n) {
         int last = -1, ans = 0;
         for (int i = 0; n != 0; i++) {
-            if ((n & 1) == 1) {
+            if ((n & 1) == 1) { // n 和 1 做与运算，可以判断 n 的最低为是否为 1
                 if (last != -1) {
-                    ans = Math.max(ans, i - last);
+                    ans = Math.max(ans, i - last); // 更新结果
                 }
                 last = i;
             }
-            n >>= 1;
+            n >>= 1; // n 不断右移
         }
         return ans;
     }
