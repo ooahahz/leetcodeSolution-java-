25e89cceae6b15410a935127725688af35a13b7a
haohao <z1446722092@gmail.com>
2022-04-28 23:32:45 +0800

    [905] 按奇偶排序数组

---
+++ 905.按奇偶排序数组.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=905 lang=java
+ *
+ * [905] 按奇偶排序数组
+ * 双指针
+ * 开辟一个数组空间
+ * 遍历原数组，遇到偶数移动左指针，遇到奇数移动右指针，直至遍历完数组
+ */
+
+// @lc code=start
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        int left = 0, right = n - 1;
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                res[left++] = num;
+            } else {
+                res[right--] = num;
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
